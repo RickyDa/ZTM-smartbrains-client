5e317dbdf1a60c9cc2e7305f9b1f7d15eac4c7ff
Ricky Danipog <Ricky.Danipog4@gmail.com>
2020-12-07 17:27:54 +0200

    some editing

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import './ImageLinkForm.css'
 const ImageLinkForm = ({onInputChange, onSubmit}) => {
     return (
         <div>
-            <p className='f3'>Upload an image to try the face detector. Give it a try.</p>
+            <p className='f3'>Link an image to try the face detector. Give it a try.</p>
             <div className='center'>
                 <div className='center pa4 br3 shadow-5 form'>
                     <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
